3043f03863f99b102aec175911f27410d9ad85e4
Dwight House <dwighthouse@gmail.com>
2019-01-26 20:33:26 -0600

    Added BigInt64Array and BigUint64Array types and tests.

--- src/types/BigIntType.js
+++ src/types/BigIntType.js
@@ -1,9 +1,20 @@
 import genPrimitive from '/utils/genPrimitive.js';
+import genTypedArray from '/utils/genTypedArray.js';
 
 export default (typeObj) => {
     /* istanbul ignore else */
     if (typeof BigInt === 'function') {
         typeObj.Bi = genPrimitive(BigInt);
+    }
+
+    /* istanbul ignore else */
+    if (typeof BigInt64Array === 'function') {
+        typeObj.BI = genTypedArray(BigInt64Array);
+    }
+
+    /* istanbul ignore else */
+    if (typeof BigUint64Array === 'function') {
+        typeObj.BU = genTypedArray(BigUint64Array);
     }
 
     return typeObj;
--- src/types/TypedArrayTypes.js
+++ src/types/TypedArrayTypes.js
@@ -1,23 +1,4 @@
-import attachIndices from '/utils/attachIndices.js';
-import attachKeys from '/utils/attachKeys.js';
-import encodeWithAttachments from '/utils/encodeWithAttachments.js';
-import getSystemName from '/utils/getSystemName.js';
-
-const genTypedArray = (type) => {
-    return {
-        _systemName: getSystemName(new type()),
-        _encodeValue: (reference, attachments) => {
-            return encodeWithAttachments([attachments._indices], attachments);
-        },
-        _generateReference: (store, dataItems) => {
-            return new type(dataItems[0].length);
-        },
-        _build: (store, dataItem) => {
-            attachIndices(store, dataItem);
-            attachKeys(store, dataItem, 1, 2);
-        },
-    };
-};
+import genTypedArray from '/utils/genTypedArray.js';
 
 export default (typeObj) => {
     // If an environment supports Int8Array, it will support most of the TypedArray types
